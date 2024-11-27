2c72e966a13cccf9828fbeb32c9d091ec9dfdcd8
NinSam <ninsamgd@gmail.com>
2024-11-27 14:15:15 -0800

    three randomized evw voicelines

--- changelog.md
+++ changelog.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## v1.0.1
+- Clicking the recent tab will now play three randomized evw voicelines (Suggestion from [hiimjustin000](user:7466002))
+
 ## v1.0.0
 - Initial Release
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Geode/Geode.hpp>
+#include <ctime>
 #include <Geode/modify/LevelSearchLayer.hpp>
 
 using namespace geode::prelude;
@@ -8,31 +9,30 @@ class $modify(LevelSearchLayer) {
 	void onMostRecent(CCObject* sender){
 
 		LevelSearchLayer::onMostRecent(sender);
+
 		if (Mod::get()->getSettingValue<bool>("enable-voiceline")){
 
-			FMODAudioEngine::sharedEngine()->playEffect("evw_recent_tab.ogg"_spr);
+		srand(time(0));
+		int random = rand() % 3 + 1;
+
+		switch(random){
+			
+			case 1: FMODAudioEngine::sharedEngine()->playEffect("evw_recent_tab.ogg"_spr);
+
+			break;
+
+			case 2: FMODAudioEngine::sharedEngine()->playEffect("evw_recent_tab_1.ogg"_spr);
+
+			break;
+
+			case 3: FMODAudioEngine::sharedEngine()->playEffect("evw_recent_tab_2.ogg"_spr);
+
+			break;
+
 		}
+			
+		}
+
 	}
-
-	/*
-	
-
-	Idea from @Random_person921
-
-	from here: https://www.youtube.com/watch?v=khNOxO_uq80&t=25s
-	
-	Uses music slider: FMODAudioEngine::sharedEngine()->playMusic();
-
-	Uses sfx slider: FMODAudioEngine::sharedEngine()->playEffect();
-
-
-
-
-    Get bool setting: Mod::get()->getSettingValue<bool>("")
-
-	Get int setting: Mod::get()->getSettingValue<int64_t>("") == 0
-	
-
-	*/
 
 };
